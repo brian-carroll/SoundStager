ee690227e6605fa2d35eceb8ed12a6f068546f51
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-03-25 20:14:07 +0000

    Switch to table-based layout for simplicity

--- src/View.elm
+++ src/View.elm
@@ -29,7 +29,7 @@ root =
 
 soundList : List String -> Html msg
 soundList fileNames =
-    div
+    table
         [ style
             [ ( "text-align", "center" )
             , ( "max-width", "300px" )
@@ -37,60 +37,32 @@ soundList fileNames =
             ]
         ]
         (separator
-            :: (List.map soundWrapper fileNames)
+            :: List.concat (List.map soundWrapper fileNames)
         )
 
 
-soundWrapper : String -> Html msg
+soundWrapper : String -> List (Html msg)
 soundWrapper fileName =
-    div []
-        [ (sound fileName)
-        , separator
+    [ tr []
+        [ td []
+            [ sound fileName ]
         ]
+    , separator
+    ]
 
 
 separator : Html msg
 separator =
-    div []
-        [ dropZone
-        , div
-            [ style
-                [ ( "display", "inline-block" )
-                , ( "width", "20%" )
-                , ( "background-color", "red" )
-                  -- , ( "padding-top", "10px" )
-                  -- , ( "padding-bottom", "10px" )
-                ]
-            ]
+    tr []
+        [ td [] []
+        , td []
             [ button [] [ text "+" ]
             ]
         ]
 
 
-dropZone : Html msg
-dropZone =
-    div
-        [ style
-            [ ( "padding-top", "10px" )
-            , ( "padding-bottom", "10px" )
-            , ( "background-color", "blue" )
-            , ( "display", "inline-block" )
-            , ( "width", "80%" )
-            ]
-        ]
-        []
-
-
 sound : String -> Html msg
 sound fileName =
     div
-        [ style
-            [ ( "padding-top", "20px" )
-            , ( "padding-bottom", "20px" )
-            , ( "width", "80%" )
-            , ( "display", "inline-block" )
-            , ( "border", "1px solid black" )
-            , ( "float", "left" )
-            ]
-        ]
+        [ class "sound" ]
         [ text fileName ]
